6e157a5ef27d1bef9adcf1798d347440d3f77241
PureJoyMind <mazdak.parniandg@gmail.com>
2023-04-22 15:50:03 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack(10);
+
+            stack.Push("mazdak");
+            stack.Push(DateTime.Today);
+
+            stack.PrintStack();
+
+            Console.WriteLine(stack.Pop());
+            stack.PrintStack();
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Stack
+    {
+        private object[] items;
+        private int top;
+
+        public Stack(int capacity)
+        {
+            items = new object[capacity];
+            top = 0;
+        }
+
+        public void Push(object item)
+        {
+            if (top == items.Length - 1)
+            {
+                throw new StackOverflowException();
+            }
+
+            top++;
+            items[top] = item;
+        }
+
+        public object Pop()
+        {
+            if (top == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var item = items[top];
+            top--;
+            return item;
+        }
+
+        public void Clear()
+        {
+            top = 0;
+        }
+
+        public void PrintStack()
+        {
+            if (top == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+
+            Console.Write("Stack: ");
+            for (int i = top; i >= 0; i--)
+            {
+                Console.Write(items[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
