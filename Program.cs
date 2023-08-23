c28c6ad78238f5bedbc828a337fd891c999f71ba
PureJoyMind <mazdak.parniandg@gmail.com>
2023-08-23 23:57:07 +0330

    Added Bubble sort

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+using System.Linq;
+using static System.Console;
 
 namespace DataStructures
 {
@@ -7,10 +9,25 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var stack = new Stack(5);
+            //var stack = new Stack(5);
+            //stack.Implementation();
 
-            stack.Implementation();
+            var arr = new int[] {12, 5, 2, 8, 10, 0, 13, 5, 7, 6, 4};
+            Write("our array before sort: ");
+            PrintArray(arr);
+            Sort.BubbleSort(arr);
+            Write("our array after sort: ");
+            PrintArray(arr);
         }
 
+
+        public static void PrintArray(params int[] arr)
+        {
+            foreach (var i in arr)
+            {
+                Write(i + ", ");
+            }
+            WriteLine();
+        }
     }
 }
---
+++ Sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Sort
+    {
+        public static void BubbleSort(params int[] arr)
+        {
+            if (arr == null) throw new ArgumentNullException(nameof(arr));
+            if (arr.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(arr));
+
+            bool isSorted;
+
+            for (int i = 0; i < arr.Length ; i++)
+            {
+                isSorted = true;
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        isSorted = false;
+                    }
+                }
+
+                if (isSorted)
+                {
+                    return; 
+                }
+            }
+        }
+    }
+}
